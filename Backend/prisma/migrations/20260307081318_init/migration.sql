d636490e756ea0a29e69ad30591d0c17a5270fc1
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2026-03-08 22:44:09 +0100

    Ristrutturazione della parte backend del sito; implementazione iniziale del server Express

---
+++ Backend/prisma/migrations/20260307081318_init/migration.sql
@@ -0,0 +1,171 @@
+-- CreateEnum
+CREATE TYPE "Stato" AS ENUM ('In bozza', 'Validato', 'Scaduto');
+
+-- CreateEnum
+CREATE TYPE "Tipologia_Doc" AS ENUM ('DSA', 'BES');
+
+-- CreateEnum
+CREATE TYPE "Tipologia_Ind" AS ENUM ('DSA', 'BES', 'Entrambi');
+
+-- CreateEnum
+CREATE TYPE "Categoria" AS ENUM ('Strumenti compensativi', 'Misure dispensative', 'Modalità di verifica', 'Criteri di valutazione');
+
+-- CreateTable
+CREATE TABLE "Docente" (
+    "Email" VARCHAR(255) NOT NULL,
+    "Nome" VARCHAR(50) NOT NULL,
+    "Cognome" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "Docente_pkey" PRIMARY KEY ("Email")
+);
+
+-- CreateTable
+CREATE TABLE "Classe" (
+    "Id" SERIAL NOT NULL,
+    "Classe" INTEGER NOT NULL,
+    "Sezione" CHAR(1) NOT NULL,
+    "Indirizzo" VARCHAR(5) NOT NULL,
+    "Anno_Scolastico" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Classe_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateTable
+CREATE TABLE "Studente" (
+    "Email" VARCHAR(255) NOT NULL,
+    "Nome" VARCHAR(50) NOT NULL,
+    "Cognome" VARCHAR(50) NOT NULL,
+    "DSA_BES" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Studente_pkey" PRIMARY KEY ("Email")
+);
+
+-- CreateTable
+CREATE TABLE "Documento" (
+    "Studente_Email" VARCHAR(255) NOT NULL,
+    "Anno" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "Stato" "Stato" NOT NULL,
+    "Tipologia" "Tipologia_Doc" NOT NULL,
+    "Data_Approvazione" TIMESTAMP(3),
+
+    CONSTRAINT "Documento_pkey" PRIMARY KEY ("Studente_Email","Anno")
+);
+
+-- CreateTable
+CREATE TABLE "Materia" (
+    "Nome" VARCHAR(50) NOT NULL,
+
+    CONSTRAINT "Materia_pkey" PRIMARY KEY ("Nome")
+);
+
+-- CreateTable
+CREATE TABLE "Indicatore" (
+    "Id" SERIAL NOT NULL,
+    "Tipologia" "Tipologia_Ind" NOT NULL,
+    "Categoria" "Categoria" NOT NULL,
+    "Descrizione" TEXT NOT NULL,
+
+    CONSTRAINT "Indicatore_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateTable
+CREATE TABLE "Allegato" (
+    "Id" SERIAL NOT NULL,
+    "Nome" TEXT NOT NULL,
+    "Percorso" TEXT NOT NULL,
+    "Documento_Studente_Email" TEXT NOT NULL,
+    "Documento_Anno" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Allegato_pkey" PRIMARY KEY ("Id")
+);
+
+-- CreateTable
+CREATE TABLE "ICF" (
+    "Codice" TEXT NOT NULL,
+    "Descrizione" TEXT,
+
+    CONSTRAINT "ICF_pkey" PRIMARY KEY ("Codice")
+);
+
+-- CreateTable
+CREATE TABLE "Insegnamento" (
+    "Docente_Email" TEXT NOT NULL,
+    "Classe_Id" INTEGER NOT NULL,
+    "Materia_Nome" TEXT NOT NULL,
+
+    CONSTRAINT "Insegnamento_pkey" PRIMARY KEY ("Docente_Email","Classe_Id","Materia_Nome")
+);
+
+-- CreateTable
+CREATE TABLE "Classe_Studente" (
+    "Classe_Id" INTEGER NOT NULL,
+    "Studente_Email" TEXT NOT NULL,
+
+    CONSTRAINT "Classe_Studente_pkey" PRIMARY KEY ("Studente_Email","Classe_Id")
+);
+
+-- CreateTable
+CREATE TABLE "Materia_Indicatore" (
+    "Materia_Nome" TEXT NOT NULL,
+    "Indicatore_Id" INTEGER NOT NULL,
+    "Valore" BOOLEAN NOT NULL,
+
+    CONSTRAINT "Materia_Indicatore_pkey" PRIMARY KEY ("Materia_Nome","Indicatore_Id")
+);
+
+-- CreateTable
+CREATE TABLE "Materia_Documento" (
+    "Materia_Nome" TEXT NOT NULL,
+    "Documento_Anno" TIMESTAMP(3) NOT NULL,
+    "Documento_Studente_Email" TEXT NOT NULL,
+
+    CONSTRAINT "Materia_Documento_pkey" PRIMARY KEY ("Materia_Nome","Documento_Anno","Documento_Studente_Email")
+);
+
+-- CreateTable
+CREATE TABLE "Documento_ICF" (
+    "ICF_Codice" TEXT NOT NULL,
+    "Documento_Anno" TIMESTAMP(3) NOT NULL,
+    "Documento_Studente_Email" TEXT NOT NULL,
+
+    CONSTRAINT "Documento_ICF_pkey" PRIMARY KEY ("ICF_Codice","Documento_Anno","Documento_Studente_Email")
+);
+
+-- AddForeignKey
+ALTER TABLE "Documento" ADD CONSTRAINT "Documento_Studente_Email_fkey" FOREIGN KEY ("Studente_Email") REFERENCES "Studente"("Email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Allegato" ADD CONSTRAINT "Allegato_Documento_Studente_Email_Documento_Anno_fkey" FOREIGN KEY ("Documento_Studente_Email", "Documento_Anno") REFERENCES "Documento"("Studente_Email", "Anno") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Insegnamento" ADD CONSTRAINT "Insegnamento_Docente_Email_fkey" FOREIGN KEY ("Docente_Email") REFERENCES "Docente"("Email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Insegnamento" ADD CONSTRAINT "Insegnamento_Classe_Id_fkey" FOREIGN KEY ("Classe_Id") REFERENCES "Classe"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Insegnamento" ADD CONSTRAINT "Insegnamento_Materia_Nome_fkey" FOREIGN KEY ("Materia_Nome") REFERENCES "Materia"("Nome") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Classe_Studente" ADD CONSTRAINT "Classe_Studente_Classe_Id_fkey" FOREIGN KEY ("Classe_Id") REFERENCES "Classe"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Classe_Studente" ADD CONSTRAINT "Classe_Studente_Studente_Email_fkey" FOREIGN KEY ("Studente_Email") REFERENCES "Studente"("Email") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materia_Indicatore" ADD CONSTRAINT "Materia_Indicatore_Materia_Nome_fkey" FOREIGN KEY ("Materia_Nome") REFERENCES "Materia"("Nome") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materia_Indicatore" ADD CONSTRAINT "Materia_Indicatore_Indicatore_Id_fkey" FOREIGN KEY ("Indicatore_Id") REFERENCES "Indicatore"("Id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materia_Documento" ADD CONSTRAINT "Materia_Documento_Materia_Nome_fkey" FOREIGN KEY ("Materia_Nome") REFERENCES "Materia"("Nome") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Materia_Documento" ADD CONSTRAINT "Materia_Documento_Documento_Studente_Email_Documento_Anno_fkey" FOREIGN KEY ("Documento_Studente_Email", "Documento_Anno") REFERENCES "Documento"("Studente_Email", "Anno") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Documento_ICF" ADD CONSTRAINT "Documento_ICF_ICF_Codice_fkey" FOREIGN KEY ("ICF_Codice") REFERENCES "ICF"("Codice") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Documento_ICF" ADD CONSTRAINT "Documento_ICF_Documento_Studente_Email_Documento_Anno_fkey" FOREIGN KEY ("Documento_Studente_Email", "Documento_Anno") REFERENCES "Documento"("Studente_Email", "Anno") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ Backend/src/server.ts
@@ -0,0 +1,102 @@
+//A. import delle librerie
+import https from "https";
+import fs from "fs";
+import express from "express";
+import dotenv from "dotenv";
+import queryStringParser from "./queryStringParser.js";
+import cors from "cors";
+
+import { PrismaClient } from "../prisma/generated/client/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
+
+export const prisma = new PrismaClient({ adapter }); //export così da poterlo usare nelle API route (root dinamiche)
+
+//B. configurazioni
+//riconosce i tipi automaticamente (non è any) -> grazie @types/node in devDependencies (sviluppo)
+const app = express();
+//stessa cosa -> const app: express.Express = express();
+dotenv.config({ path: "../.env" });
+const https_port = process.env.HTTPS_PORT;
+
+//Lettura pagina errore
+// let paginaErr = "";
+
+// fs.readFile("./static/error.html", function(err, content){ //content è una sequenza di byte
+//     if(err)
+//         paginaErr = "<h1>Risorsa non trovata</h1>";
+//     else
+//         paginaErr = content.toString();
+// })
+
+//C. creazione ed avvio di un server https
+const privateKey = fs.readFileSync("keys/privateKey.pem", "utf8");
+const certificate = fs.readFileSync("keys/certificate.crt", "utf8");
+const credentials = { "key": privateKey, "cert": certificate }; 
+
+let httpsServer = https.createServer(credentials, app);
+httpsServer.listen(https_port, function(){
+ console.log("Server in ascolto sulla porta HTTPS:" + https_port)
+});
+
+//D. middleware
+//middleware 1: request log
+app.use(function(req, res, next) //se si omette => come risorsa "/"
+{
+    console.log("Ricevuta richiesta: " + req.method + ": " + req.originalUrl);
+    next(); //passa al middleware successivo
+});
+
+//middleware 2: gestione delle risorse statiche
+app.use(express.static("./static"));
+
+//middleware 3: gestione dei parametri post
+app.use(express.json({"limit": "5mb"})); //i parametri post sono restituiti in req.body
+//i parametri get invece sono restituiti come json in req.query
+
+//middleware 4: parsing dei parametri GET
+app.use("/", queryStringParser);
+
+//middleware 5: log dei parametri
+app.use((req: any, res, next) => {
+    if(req.body && Object.keys(req.body).length > 0)
+        console.log("   Parametri body: " + JSON.stringify(req.body));
+
+    if (req["parsedQuery"] && Object.keys(req["parsedQuery"]).length > 0)
+        console.log("   Parametri query: " + JSON.stringify(req["parsedQuery"]));
+
+    next();
+});
+
+//middleware 6: Vincoli CORS (controlli lato server che consentono di accettare richieste anche da fuori dal dominio -> cioè diverso dal server da cui arrivano le pagine)
+const corsOptions = {
+    origin: function(origin: any, callback: any) {
+        return callback(null, true);
+    },
+    credentials: true
+};
+app.use("/", cors(corsOptions));
+
+
+//E. gestione delle root dinamiche
+
+
+//F. default root e gestione errori
+app.use(function(req, res){    
+    if(req.originalUrl.startsWith("/api/"))
+        res.status(404).send("Risorsa non trovata");
+    else if(req.accepts("html")) // se la richiesta è per una pagina html
+        res.status(404).send("<h1>Pagina non trovata</h1>");
+    else
+        res.sendStatus(404);
+        // res.status(404).send(); equivalente
+});
+
+//G. gestione errori
+app.use(function(err: Error, req: express.Request, res: express.Response, next: express.NextFunction){
+    console.error("*** ERRORE ***:\n" + err.stack); //elenco completo degli errori
+    res.status(500).send("Errore interno del server");
+});
