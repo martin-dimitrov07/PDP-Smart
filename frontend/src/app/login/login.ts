c272d0f5d1f17e1c7d57d259996639f59ccf2653
unknown <m.dimitrov.3065@vallauri.edu>
2026-03-21 19:12:22 +0100

    Implementata modalità scura sul tema del sito

--- frontend/src/app/login/login.ts
+++ frontend/src/app/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { LoginCard } from './login-card/login-card';
 import { LoginForm } from './login-form/login-form';
 
@@ -10,4 +10,25 @@ import { LoginForm } from './login-form/login-form';
 })
 export class Login {
 
+    isDark: boolean = false;
+    @ViewChild("icon") themeIcon: any;
+
+    ngAfterViewInit(){
+        this.themeIcon = document.querySelector(".btn-theme .bi");
+    }
+
+    async ChangeTheme(){
+        this.isDark = !this.isDark;
+        document.body.classList.toggle("dark-mode");
+
+        const icon = this.themeIcon.nativeElement;
+
+        if(this.isDark){
+            icon.classList.remove("bi-moon-fill");
+            icon.classList.add("bi-sun-fill");
+        } else {
+            icon.classList.remove("bi-sun-fill");
+            icon.classList.add("bi-moon-fill");
+        }
+    }
 }
