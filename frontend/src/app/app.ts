53714a619ec3bf9f315325cc0ae41be5ab52a33d
martin-dimitrov07 <m.dimitrov.3065@vallauri.edu>
2026-03-21 15:14:28 +0100

    Implementate modifiche e migliorie alla pagina login

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { Login } from './login/login';
+import { Studenti } from './studenti/studenti';
 
 @Component({
     selector: 'app-root',
-    imports: [RouterOutlet, Header, Login],
+    imports: [RouterOutlet, Header, Login, Studenti],
     templateUrl: './app.html',
     styleUrl: './app.css'
 })
---
+++ frontend/src/app/studenti/studenti.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-studenti',
+  imports: [],
+  templateUrl: './studenti.html',
+  styleUrl: './studenti.css',
+})
+export class Studenti {
+
+}
