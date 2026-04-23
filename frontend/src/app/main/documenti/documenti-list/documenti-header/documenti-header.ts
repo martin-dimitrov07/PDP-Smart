8ce8c684e8278c60288adf829fd2f18774ba0782
Lorenzo <l.martino.3175@vallauri.edu>
2026-04-23 23:22:28 +0200

    Aggiunto componente documenti-header

---
+++ frontend/src/app/main/documenti/documenti-list/documenti-header/documenti-header.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documenti-header',
+  imports: [],
+  templateUrl: './documenti-header.html',
+  styleUrl: './documenti-header.css',
+})
+export class DocumentiHeader {
+
+}
--- frontend/src/app/main/documenti/documenti-list/documenti-list.ts
+++ frontend/src/app/main/documenti/documenti-list/documenti-list.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { DocumentiCard } from "./documenti-card/documenti-card";
+import { DocumentiHeader } from "./documenti-header/documenti-header";
 
 @Component({
   selector: 'app-documenti-list',
-  imports: [DocumentiCard],
+  imports: [DocumentiCard, DocumentiHeader],
   templateUrl: './documenti-list.html',
   styleUrl: './documenti-list.css',
 })
