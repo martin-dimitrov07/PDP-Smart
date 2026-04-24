e0b6dbb3815ca5f5405965806147f5955981ab6f
Lorenzo <l.martino.3175@vallauri.edu>
2026-04-24 20:56:52 +0200

    Migliorato responsive e colori pagina documenti e componente documenti-card

---
+++ frontend/src/app/main/documenti/documenti-list/documenti-filters/documenti-filters.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-documenti-filters',
+  imports: [],
+  templateUrl: './documenti-filters.html',
+  styleUrl: './documenti-filters.css',
+})
+export class DocumentiFilters {
+
+}
