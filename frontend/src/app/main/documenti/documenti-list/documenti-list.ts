c4eb08a02ea1ebf68e3f3d5f4ac8404573704c51
Lorenzo <l.martino.3175@vallauri.edu>
2026-04-25 18:22:41 +0200

    Aggiunta sezione documenti-filters (solo grafica)

--- frontend/src/app/main/documenti/documenti-list/documenti-list.ts
+++ frontend/src/app/main/documenti/documenti-list/documenti-list.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { DocumentiCard } from "./documenti-card/documenti-card";
 import { DocumentiHeader } from "./documenti-header/documenti-header";
+import { DocumentiFilters } from "./documenti-filters/documenti-filters";
 
 @Component({
   selector: 'app-documenti-list',
-  imports: [DocumentiCard, DocumentiHeader],
+  imports: [DocumentiCard, DocumentiHeader, DocumentiFilters],
   templateUrl: './documenti-list.html',
   styleUrl: './documenti-list.css',
 })
